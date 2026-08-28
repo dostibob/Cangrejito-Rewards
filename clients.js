7eee3d770b83cb77c339d723accc63fd615b815b
dostibob <adriankesito@gmail.com>
2026-08-28 11:01:22 -0600

    Add files via upload

---
+++ clients.js
@@ -0,0 +1,15 @@
+// Base de datos de clientes
+const CLIENTS_DB = {
+  "c1": {
+    name: "Eimy Roman",
+    stamps: 0
+  },
+  "c2": {
+    name: "Carlos Salazar",
+    stamps: 0
+  },
+  "c3": {
+    name: "Camila Mermelada",
+    stamps: 0
+  }
+};
